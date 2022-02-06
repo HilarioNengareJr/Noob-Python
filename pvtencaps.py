64ff6a6979ac695b5236c1788db2875131ff56fa
HilarioNengareJr <38634516+HilarioNengareJr@users.noreply.github.com>
2022-02-06 18:40:15 +0200

    Create pvtencaps.py

---
+++ pvtencaps.py
@@ -0,0 +1,23 @@
+
+class A:
+    def __init__(self):
+        self._name = 'hilario nengare' #protected
+        self.__salary = '$250_000'  #private
+        
+    def accessor(self):
+        print(f"Name is {self.name}")
+        print(f"Salary is {self.salary}")
+        
+class B(A):
+    def __init__(self):
+        A.__init__(self) #calling Base ctor
+        print(f"Name is {self._name}")
+        print(f"Salary is {self._salary}") #error: attribute error
+        
+#inside main
+a = A()
+print(a._name) #prints protected name
+#print(a.__salary)
+b = B()#prints accessible data member
+print(b._name) #accesses the protected data member
+
